6aafad19ba6a6bcd1529b6aae21aad662ec91869
salmonMaxx <maxxwallberg@gmail.com>
2019-11-26 18:30:24 +0900

    MusicMultheadAttention: added functions: transform_input() and matrix_to_heads(). There is a mismatch error in matrix_to_heads still because of the matrix multiplication.

--- attention.py
+++ attention.py
@@ -18,15 +18,61 @@ def square_tensor(input, fill=0.5):
 #        torch.nn.Module.__init__(self)
 
 class MusicMultiheadAttention(torch.nn.MultiheadAttention):
-    def __init__(self, d_model, nhead, dropout=0.1):
-        torch.nn.MultiheadAttention.__init__(self, embed_dim, num_heads,
-                                             dropout=0., bias=True,
-                                             add_bias_kv=False, add_zero_attn=False,
-                                             kdim=None, vdim=None)
+    def __init__(self, embed_dim, nhead, dropout=0.1, bias=True, add_bias_kv=False, 
+                 add_zero_attn=False, kdim=None, vdim=None):
+        torch.nn.MultiheadAttention.__init__(self, embed_dim, nhead, dropout=0.1, 
+                                             bias=True, add_bias_kv=False, 
+                                             add_zero_attn=False, kdim=None, vdim=None)
 
-    def forward():
-        # Implement forward method.
+    def forward(self, query, key, value):
+        Q, K, V = self.transform_input(query, key, value)
+        # Reshaping the matrices 
+        # Each L × D query, key, and value matrix is then split into H L × D 
+        # h_D parts or attention heads, indexed by h, and with dimension D_h = D/H
+        Q = self.matrix_to_heads(Q)
+        K   = self.matrix_to_heads(K)
+        V = self.matrix_to_heads(V)
+        
+        z_attention = attention(Q, K, V)
+        
         return None
+
+    def attention(weights_q, weights_k, weights_v):
+        return None
+    
+    def matrix_to_heads(self, qkv):
+        '''
+            Takes a query/key/value (qkv) matrix and reshapes it to  H LxD_h heads 
+            with dimension D_h = D/H
+        '''
+        batch_size_q = qkv.size(0)
+        #print(qkv.size())
+        qkv = torch.reshape(qkv, (batch_size_q, qkv.size(0), self.num_heads, self.head_dim))
+        #print(self.head_dim // self.num_heads)
+        #print(qkv.size())
+        return qkv
+
+    def transform_input(self, query, key, value):
+        '''
+            Transforming the input vector, X, of LxD dimension 
+            into 
+                queries: Q = XW^Q 
+                keys:    K = XW^K
+            and values:  V = XW^V
+            which are all DxD square matrices.
+        '''
+        l_query = query.size(1)
+        weights_q = torch.nn.Linear(l_query, l_query)
+        
+        #print(L_q)
+        l_key = key.size(1)
+        weights_k = torch.nn.Linear(l_key, l_key)
+        
+        l_value = key.size(1)
+        weights_v = torch.nn.Linear(l_value, l_value)
+        
+        return weights_q(query), weights_k(key), weights_v(value)
+    
 
 class MusicTransformerEncoderLayer(torch.nn.TransformerEncoderLayer):
     def __init__(self, d_model, nhead, dim_feedforward=2048,
@@ -42,7 +88,14 @@ class MusicTransformerDecoderLayer(torch.nn.TransformerDecoderLayer):
                  # OVERRIIIIIIDE
                  self.self_attn = MusicMultiheadAttention(d_model, nhead)
 
-bob = torch.nn.Transformer(custom_encoder=MusicTransformerEncoderLayer(512, 8),
-                     custom_decoder=MusicTransformerDecoderLayer(512, 8))
-print(bob)
+#bob = torch.nn.Transformer(custom_encoder=MusicTransformerEncoderLayer(512, 8),
+#                     custom_decoder=MusicTransformerDecoderLayer(512, 8))
+#print(bob)
 #print(square_tensor(input))
+tensor = torch.ones((4,4,512))
+#print(tensor)
+
+mma = MusicMultiheadAttention(512, 8) 
+#print(mma)
+
+mma.forward(tensor, tensor, tensor)
